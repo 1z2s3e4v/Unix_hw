6d71b793577bc8ee474c20ad9557ddfd3aa6cd5c
TeRRy <terry@TeRRyde-MacBook-Pro.local>
2019-06-06 02:16:30 +0800

    fix crontab

--- page_xcs/php/exec.php
+++ page_xcs/php/exec.php
@@ -16,8 +16,8 @@ $_passwd = $_POST["_pwd"];
 $_message = $_POST["_mes"];
 
 
-$page = shell_exec("sh/./remind.sh '".$_Min."' '".$_Hour."' '".$_Day."' '".$_Mon."' '".$_PC."' '".$_User."' '".$_passwd."' '".$_message."' 2>&1");
-echo "xcow";
+$page = shell_exec("../sh/./remind.sh '".$_Min."' '".$_Hour."' '".$_Day."' '".$_Mon."' '".$_PC."' '".$_User."' '".$_passwd."' '".$_message."' 2>&1");
+echo "page";
 
 ?>
 
