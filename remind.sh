d81bc9b3ea52b41b522527091f32d0b916e3c70e
1z2s3e4v <chfrank9807@gmail.com>
2019-06-05 20:17:22 +0800

    finish the script

--- remind.sh
+++ remind.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-#./remind $passwd $username $ip 
-sshpass -p 'qwerasdf' ssh -o StrictHostKeyChecking=no s1041464@140.138.152.108 "export DISPLAY=:0.0;xcowsay HI"
+#./remind $minute $hour $day $month $passwd $username $ip $message
+crontab -l > /tmp/cron.tmp 
+cmd="sshpass -p \"$5\" ssh -o StrictHostKeyChecking=no $6@$7 \"export DISPLAY=:0.0;xcowsay $8\""
+#sshpass -p "$5" ssh -o StrictHostKeyChecking=no $6@$7 "export DISPLAY=:0.0;xcowsay $8"
+echo "$1 $2 $3 $4 * $cmd" >> /tmp/cron.tmp
+crontab /tmp/cron.tmp
+rm /tmp/cron.tmp
