c8a1e9714e82b1e6936c8cc7604795bcd0271783
TeRRy <terry@133-17.wlan.yzu.edu.tw>
2019-06-06 00:58:18 +0800

    php update about sh

--- page_xcs/js/main.js
+++ page_xcs/js/main.js
@@ -10,9 +10,24 @@ function main(){
 function connect(){
   alert("hello!");
 
+
+  var time = $("#time").val();
+  var PC = $("#PC").val();
+  var User = $("#User").val();
+  var passwd = $("#passwd").val();
+  var message = $("#message").val();
+
+  alert("time: " + time);
+
+  alert("PC: " + PC);
+  alert("User: " + User);
+  alert("passwd: " + passwd);
+  alert("message: " + message);
+
   $.ajax({
 
       url: "php/exec.php",
+      data: {time: time,PC: PC, User: User, passwd: passwd, message: message},
 
       type: "POST",
       datatype: "html",
--- page_xcs/php/exec.php
+++ page_xcs/php/exec.php
@@ -1,5 +1,13 @@
 <?php  
 
+$time = $_POST["time"];
+$PC = $_POST["PC"];
+$User = $_POST["User"];
+$passwd = $_POST["passwd"];
+$message = $_POST["message"];
+
+
+$page = shell_exec("sh/./remind.sh '".$time."' '".$PC."' '".$User."' '".$passwd."' '".$message."'");
 echo "xcow";
 
 ?>
