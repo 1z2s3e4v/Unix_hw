5cf14d5dbbb119155388ac1228c0f93a4bf95749
Cheng-Yu Chiang <chfrank9807@gmail.com>
2019-06-09 00:33:59 +0800

    littlebug

--- page_xcs/sh/remind.sh
+++ page_xcs/sh/remind.sh
@@ -2,7 +2,7 @@
 
 #./remind.sh $minute $hour $day $month $passwd $username $ip $message
 crontab -l > /tmp/cron.tmp 
-cmd="sshpass -p \"$5\" ssh -o StrictHostKeyChecking=no $6@$7 \"export DISPLAY=:0.0;xcowsay $8\""
+cmd="sshpass -p \"$5\" ssh -o StrictHostKeyChecking=no $6@$7 \"export DISPLAY=:0.0;xcowsay \'$8\'\""
 #sshpass -p "$5" ssh -o StrictHostKeyChecking=no $6@$7 "export DISPLAY=:0.0;xcowsay \'$8\'"
 echo "$1 $2 $3 $4 * $cmd" >> /tmp/cron.tmp
 crontab /tmp/cron.tmp
